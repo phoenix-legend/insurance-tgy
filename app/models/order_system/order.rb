f64a3e9877f450431f40d91fa6dee915d7eb551f
beslow <549174542@qq.com>
2015-05-10 17:49:24 +0800

    user_info、order model的方法

--- app/models/order_system/order.rb
+++ app/models/order_system/order.rb
@@ -2,12 +2,16 @@ class OrderSystem::Order < ActiveRecord::Base
   belongs_to :product, :class_name => '::OrderSystem::Product'
   belongs_to :user_info, :class_name => '::UserSystem::UserInfo'
 
+  validates_presence_of :product, message: "产品不存在。"
+  validates_presence_of :user_info, message: "客户不存在。"
 
   def self.create_order options
-      options = get_arguments_options options, [:product_id, :user_info_id, :status], status: 1
+      options = get_arguments_options options, [:product_id, :user_info_id]
       order = self.new options
       order.save!
       order.reload
+      # TODO 将信息提交到合作伙伴网址。同时在数据库中记录是否提交成功
+
       order
   end
 
