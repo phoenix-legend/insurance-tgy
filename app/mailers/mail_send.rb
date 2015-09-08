79bb9c3a8b715c09cd5d4477cb90a7f6f9e496bb
刘晓琦 <ericliu@ikidstv.com>
2015-09-08 10:35:17 +0800

    X

--- app/mailers/mail_send.rb
+++ app/mailers/mail_send.rb
@@ -3,7 +3,9 @@ class MailSend < BaseMailer
   def send_car_user_infos  receiver, copy_receivers, record_number,zhuti, file_paths
 
     file_paths.each do |file_path|
-      attachments["#{Time.now.chinese_format}-#{record_number}条.xls"] = File.read( file_path )
+      file_name = file_path.split('/')[-1]
+      attachments["#{file_name}.xls"] = File.read( file_path )
+
     end
 
     mail( to: receiver, cc: copy_receivers,  subject:  "#{record_number}总计,#{zhuti}")
