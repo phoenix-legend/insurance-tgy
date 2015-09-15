d913a79e4aad2daf7e3d141fd89fe8bf56d11316
刘晓琦 <ericliu@ikidstv.com>
2015-09-15 16:44:28 +0800

    lib

---
+++ lib/car_user_info_parse/che168.rb
@@ -0,0 +1,14 @@
+module Che168
+  def self.get_car_user_list
+
+  end
+
+  def self.update_detail
+
+  end
+
+  def self.save_user_info
+    Che168.get_car_user_list
+    Che168.update_detail
+  end
+end
