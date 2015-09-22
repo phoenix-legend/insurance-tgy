99987ff1511f982b36d2dbc637a359e27d233298
刘晓琦 <ericliu@ikidstv.com>
2015-09-22 16:56:19 +0800

    增加锚点

--- config/routes.rb
+++ config/routes.rb
@@ -120,7 +120,7 @@ Rails.application.routes.draw do
   end
 
 
-  root to: "/index.html"
+  # root to: "wz/order_system/products#index"
   get '/cms', to: 'cms/employee_validate/functions#login'
   get '/dwz', to: 'dwz/haoche_stat/promation#index'
 
