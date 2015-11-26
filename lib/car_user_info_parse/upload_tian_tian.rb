f5c34415c810f4dc37fecb7e823aeeb7105e5700
刘晓琦 <ericliu@ikidstv.com>
2015-11-26 17:09:11 +0800

    x

--- lib/car_user_info_parse/upload_tian_tian.rb
+++ lib/car_user_info_parse/upload_tian_tian.rb
@@ -60,7 +60,7 @@ module UploadTianTian
     car_user_info.tt_error = options["error"]
     car_user_info.tt_message = options["message"]
     car_user_info.tt_result = options["result"]
-    if car_user_info.tt_code == 200 and car_user_info.tt_error == "false"
+    if car_user_info.tt_code.to_i == 200 and car_user_info.tt_error == "false"
       car_user_info.tt_upload_status = 'success'
     else
       car_user_info.tt_upload_status = 'shibai'
