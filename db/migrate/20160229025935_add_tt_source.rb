e67a3d185df67c08067e2fcf5b654ab5b3a440f5
刘晓琦 <ericliu@ikidstv.com>
2016-02-29 11:02:11 +0800

    x

---
+++ db/migrate/20160229025935_add_tt_source.rb
@@ -0,0 +1,7 @@
+class AddTtSource < ActiveRecord::Migration
+  def change
+    add_column :car_user_infos, :tt_source, :string
+    add_column :car_user_infos, :tt_created_day, :date
+    add_column :car_user_infos, :tt_yixiang_day, :date
+  end
+end
