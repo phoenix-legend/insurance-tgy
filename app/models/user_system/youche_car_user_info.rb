24a7025e3e159c8df40c793ab1643cb9972700e5
刘晓琦 <ericliu@ikidstv.com>
2016-04-27 17:09:06 +0800

    增加车行关键字

--- app/models/user_system/youche_car_user_info.rb
+++ app/models/user_system/youche_car_user_info.rb
@@ -20,47 +20,81 @@ class UserSystem::YoucheCarUserInfo < ActiveRecord::Base
 
 
 
+  def self.batch_upload_youche
+    UserSystem::YoucheCarUserInfo.where("youche_upload_status = '未上传' ").each do |yc_car_user_info|
+      upload_youche yc_car_user_info
+    end
+  end
+
+
   def self.upload_youche yc_car_user_info
 
+    yc_car_user_info.name = yc_car_user_info.name.gsub('(个人)', '')
+    yc_car_user_info.save!
+
+    if yc_car_user_info.phone_city.blank?
+      phone_city_name = get_city_name yc_car_user_info.phone
+      yc_car_user_info.phone_city = phone_city_name
+      yc_car_user_info.save!
+    end
+
+
     if yc_car_user_info.phone.blank? or yc_car_user_info.brand.blank?
-      yc_car_user_info.yc_upload_status = '信息不完整'
+      yc_car_user_info.youche_upload_status = '信息不完整'
       yc_car_user_info.save!
       return
     end
 
-    # if ['58','ganji','che168'].include? yc_car_user_info.site_name
-    #   yc_car_user_info.yc_upload_status = '先不进58，赶集,168'
-    #   yc_car_user_info.save!
-    #   return
-    # end
-
     if not CITY.include? yc_car_user_info.city_chinese
       pp '城市不对'
-      yc_car_user_info.yc_upload_status = '城市不对'
+      yc_car_user_info.youche_upload_status = '城市不对'
       yc_car_user_info.save!
       return
     end
 
     if yc_car_user_info.is_real_cheshang
       pp '车商'
-      yc_car_user_info.yc_upload_status = '车商'
+      yc_car_user_info.youche_upload_status = '车商'
       yc_car_user_info.save!
       return
     end
 
     if yc_car_user_info.is_pachong
       pp '爬虫'
-      yc_car_user_info.yc_upload_status = '爬虫'
+      yc_car_user_info.youche_upload_status = '爬虫'
       yc_car_user_info.save!
       return
     end
 
     if not yc_car_user_info.is_city_match
       pp '城市不匹配'
-      yc_car_user_info.yc_upload_status = '城市不匹配'
+      yc_car_user_info.youche_upload_status = '城市不匹配'
       yc_car_user_info.save!
       return
     end
+
+
+    if yc_car_user_info.city_chinese == '北京'
+      unless yc_car_user_info.phone_city == '北京'
+        yc_car_user_info.youche_upload_status = '北京的外地电话'
+        yc_car_user_info.save!
+        return
+      end
+    end
+  end
+
+  def self.get_city_name phone
+    begin
+      response = RestClient.get "http://life.tenpay.com/cgi-bin/mobile/MobileQueryAttribution.cgi?chgmobile=#{phone}"
+      ec = Encoding::Converter.new("gb18030", "UTF-8")
+      response = ec.convert response
+      matchs = response.match /<city>(.*)<\/city>/
+      return matchs[1].to_s.strip
+    rescue Exception => e
+      pp '获取城市出错'
+      return ''
+    end
+
 
   end
 
