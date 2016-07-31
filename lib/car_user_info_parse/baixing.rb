936a32f56f30e1848f6fc226f6dc95754052001f
刘晓琦 <ericliu@ikidstv.com>
2016-07-31 23:47:52 +0800

    X

--- lib/car_user_info_parse/baixing.rb
+++ lib/car_user_info_parse/baixing.rb
@@ -9,8 +9,8 @@ module Baixing
       begin
         pp "现在跑..百姓 #{areaname}"
         1.upto 3 do |i|
-          sleep 1
-          url = "http://#{areaid}.baixing.com/m/ershouqiche/?page=#{i}"  # url = "http://shanghai.baixing.com/m/ershouqiche/?page=1"
+          sleep 2
+          url = "http://#{areaid}.baixing.com/m/ershouqiche/?page=#{i}"  # url = "http://haerbin.baixing.com/m/ershouqiche/?page=1"
           content = RestClient.get url,{'User-Agent' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'}
           content = content.body
           # pp content
@@ -68,7 +68,7 @@ module Baixing
     begin
       puts '更新明细'
       detail_url = car_user_info.detail_url.gsub('baixing.com/ershouqiche/', 'baixing.com/m/ershouqiche/')
-      sleep 2
+      sleep 3
       response = RestClient.get(detail_url, {'User-Agent' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'})
       if response.match /此信息未通过审核/
         car_user_info.need_update = false
