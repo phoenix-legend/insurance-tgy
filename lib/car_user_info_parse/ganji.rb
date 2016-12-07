9ecee79bdd33f0e58e3448c71c13d59b42563fba
刘晓琦 <ericliu@ikidstv.com>
2016-12-08 00:51:25 +0800

    区分ip使用代理

--- lib/car_user_info_parse/ganji.rb
+++ lib/car_user_info_parse/ganji.rb
@@ -94,7 +94,7 @@ module Ganji
       begin
         pp "现在跑赶集.. #{areaname}"
         1.upto 3 do |i|
-          sleep 4+rand(4)
+          sleep 4+rand(4) if  RestClientProxy.get_local_ip != '10-19-104-142'
           url = "http://wap.ganji.com/#{areaid}/ershouche/?back=search&agent=1&deal_type=1&page=#{i}"
           # url = "http://wap.ganji.com/sh/ershouche/?back=search&agent=1&deal_type=1&page=1"
           # url = "http://wap.ganji.com/su/ershouche/?back=search&agent=1&deal_type=1&page=1"
@@ -323,7 +323,7 @@ module Ganji
 
     begin
       pp "开始跑明细 #{car_user_info.id}"
-      # sleep 4+rand(4)
+      sleep 4+rand(4) if  RestClientProxy.get_local_ip != '10-19-104-142'
       response = RestClientProxy.get(car_user_info.detail_url)
       # response = RestClientProxy.get car_user_info.detail_url, {}
       # detail_content = response.body
@@ -375,6 +375,7 @@ module Ganji
       pp '-------------------------------------'
       pp e
       pp $@
+      pp detail_content
       redis = Redis.current
       redis[car_user_info.detail_url] = 'n'
       redis.expire car_user_info.detail_url, 60
