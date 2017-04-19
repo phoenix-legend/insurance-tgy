f30e1d45e84e46ffd8609fc7a44b597090469159
刘晓琦 <ericliu@ikidstv.com>
2017-04-19 20:26:35 +0800

    x;

--- app/models/user_system/aishi_car_user_info.rb
+++ app/models/user_system/aishi_car_user_info.rb
@@ -391,7 +391,7 @@ class UserSystem::AishiCarUserInfo < ActiveRecord::Base
   # UserSystem::AishiCarUserInfo.batch_query_aishi
   def self.batch_query_aishi
 
-    UserSystem::AishiCarUserInfo.where("aishi_id is not null and id > 1907590 and (aishi_yaoyue is null or aishi_yaoyue = '未知')  and created_day > ?", Date.today - 15).find_each do |cui|
+    UserSystem::AishiCarUserInfo.where("aishi_id is not null and id > 1907590 and (aishi_yaoyue is null or aishi_yaoyue = '未知')  and created_day > ?", Date.today - 5).find_each do |cui|
       next if cui.id == 2027590
       next if cui.aishi_yaoyue == '成功'
       next if cui.aishi_yaoyue == '失败'
