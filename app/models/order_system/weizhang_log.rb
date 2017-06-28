63234dfb19bf2a6c6ce3f4556125e9791f2332e3
刘晓琦 <ericliu@ikidstv.com>
2017-06-28 10:05:56 +0800

    x

--- app/models/order_system/weizhang_log.rb
+++ app/models/order_system/weizhang_log.rb
@@ -1,7 +1,21 @@
 class OrderSystem::WeizhangLog < ActiveRecord::Base
-  validates_presence_of :vin_no, message: '请正确填写车架号'
-  validates_presence_of :engine_no, message: '请正确填写发动机号'
-  validates_format_of :vin_no, :with => EricTools::RegularConstants::VinNo, message: '请正确填写车架号', allow_blank: true
-  validates_format_of :engine_no, :with => EricTools::RegularConstants::EngineNo, message:'请正确填写发动机号' ,allow_blank: true
+  # validates_presence_of :vin_no, message: '请正确填写车架号'
+  # validates_presence_of :engine_no, message: '请正确填写发动机号'
+  # validates_format_of :vin_no, :with => EricTools::RegularConstants::VinNo, message: '请正确填写车架号', allow_blank: true
+  # validates_format_of :engine_no, :with => EricTools::RegularConstants::EngineNo, message:'请正确填写发动机号' ,allow_blank: true
+
+
+  def self.add_baixing_json_body str
+    log = OrderSystem::WeizhangLog.new contents: str
+    log.save!
+  end
+
+  def self.get_baixing_json_body
+    log = OrderSystem::WeizhangLog.last
+    return if log.blank?
+    body = log.contents
+    log.destroy!
+    return body
+  end
 
 end
