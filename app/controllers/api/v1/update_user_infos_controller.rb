59f06f7abb876c4a0fe09a4089528cc38f392074
刘晓琦 <ericliu@ikidstv.com>
2018-05-16 16:55:43 +0800

    x

--- app/controllers/api/v1/update_user_infos_controller.rb
+++ app/controllers/api/v1/update_user_infos_controller.rb
@@ -159,10 +159,12 @@ class Api::V1::UpdateUserInfosController < Api::V1::BaseController
 
   def check_guazi_shangjia
     cuis = UserSystem::GuaziCarUserInfo.where("phone = ? and guazi_yaoyue = '成功'", params[:phone]).order(id: :desc).limit(1)
+
+    cuis = UserSystem::GuaziCarUserInfo.where("phone = ? and guazi_yaoyue = '成功'", '13993156253').order(id: :desc).limit(1)
     @result = if cuis.blank?
        'none'
      else
-       cuis[0].created_at.chinese_format
+       cuis[0].car_user_info.created_at.chinese_format
      end
 
   end
