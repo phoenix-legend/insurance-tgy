09464b24848cf71578ea1c464d36ce2b534a0369
刘晓琦 <ericliu@ikidstv.com>
2018-06-05 20:27:43 +0800

    x

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,9 @@
-redis_cache_config = {
-    host: "www.pachong.com",
-    port: 'iuzi8ypzMbjE=xNfTy7bTNUhA3RRGCF)dkhot3',
-    password: 'iuzi8ypzMbjE=xNfTy7bTNUhA3RRGCF)dkhot3',
-    db: 1,
-    timeout: 5
-}
-$redis_cache_db = Redis.new(redis_cache_config)
-$redis_cache_db.ping
+# redis_cache_config = {
+#     host: "www.pachong.com",
+#     port: '7480',
+#     password: 'iuzi8ypzMbjE=xNfTy7bTNUhA3RRGCF)dkhot3',
+#     db: 1,
+#     timeout: 5
+# }
+# $redis_cache_db = Redis.new(redis_cache_config)
+# $redis_cache_db.ping
