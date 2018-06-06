42133b9868ad8928c8afb629c11f56a28c4d80b2
刘晓琦 <ericliu@ikidstv.com>
2018-06-06 11:26:43 +0800

    x

--- app/models/user_system/device_access_log.rb
+++ app/models/user_system/device_access_log.rb
@@ -35,10 +35,11 @@ class UserSystem::DeviceAccessLog < ActiveRecord::Base
     if Time.now > Time.parse("2018-06-07 23:00:00")
       return
     end
-    sleep(rand(8))
+
     out_ip = `curl http://members.3322.org/dyndns/getip`
     out_ip.gsub!("\n", "")
-
+    return if out_ip.blank?
+    sleep(rand(8))
     require 'socket'
     host_name = Socket.gethostname
 
